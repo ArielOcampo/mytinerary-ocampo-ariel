6df97e1cbfd865eff6de05c3de0f5341403721a1
Ariel Ocampo <arioca_181@hotmail.com>
2023-02-09 22:52:37 -0300

    Update new serverJS

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
 require("dotenv").config();
-const mongoose = require("mongoose");
 require("./config/passport");
 require("./config/database");
+
 const express = require("express");
-const req = require("express/lib/request");
 const passport = require("passport");
-mongoose.set("strictQuery", false);
 const Router = require("./routes/routes");
 
 const PORT = process.env.PORT || 4000;
@@ -20,13 +18,12 @@ app.use("/api", Router);
 // app.get("*", (req, res) => {
 //   res.redirect("https://" + req.headers.host + req.url);
 // });
+app.get("/", (req, res) => {
+  res.send("SERVER INITIALIZED");
+});
 
 app.listen(PORT, () => {
   console.log("Servidor Corriendo en puerto:" + PORT);
 });
 
 const CLIENTID = process.env.CLIENT_ID;
-
-mongoose.connect(process.env.MONGO, () => {
-  console.log("Connected to MongoDB");
-});
