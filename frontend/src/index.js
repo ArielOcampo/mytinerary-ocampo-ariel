ccb3f218ce0851b9b859ed7e36c1a4b2dc5b4d9c
Ariel Ocampo <arioca_181@hotmail.com>
2023-03-09 20:11:00 -0300

    Vite commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import { configureStore as createStore } from "@reduxjs/toolkit";
-import mainReducer from "./redux/reducers/mainReducer"
+import mainReducer from "./redux/reducers/mainReducer";
 
-const reduxStore = createStore({ reducer: mainReducer })
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const reduxStore = createStore({ reducer: mainReducer });
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-
   <Provider store={reduxStore}>
     <BrowserRouter>
-
       <App />
     </BrowserRouter>
-  </Provider >
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
